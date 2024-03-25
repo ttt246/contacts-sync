5f2b05989a6de1d95dd1ca3859ffe2d96a3ca741
Fredy Muñoz <fredy@munoz.im>
2024-03-25 00:36:40 -0600

    Signup/Signin with Google provider

--- auth.config.ts
+++ auth.config.ts
@@ -18,5 +18,8 @@ export const authConfig = {
       return true;
     },
   },
+  session: {
+    strategy: 'jwt'
+  },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,31 +1,34 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
+import Google from 'next-auth/providers/google';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import { getUser } from './app/lib/data';
 
-const bcrypt = require('bcrypt');
-
-export const { auth, signIn, signOut } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   ...authConfig,
   providers: [
-    Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string() })
-          .safeParse(credentials);
+    Google({
+      // async authorize(credentials) {
+      //   const parsedCredentials = z
+      //     .object({ email: z.string().email(), password: z.string() })
+      //     .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) return user;
-        }
+      //   if (parsedCredentials.success) {
+      //     const { email, password } = parsedCredentials.data;
+      //     const user = await getUser(email);
+      //     if (!user) return null;
+      //     const passwordsMatch = await bcrypt.compare(password, user.password);
+      //     if (passwordsMatch) return user;
+      //   }
 
-        console.log('Invalid credentials');
-        return null;
-      },
+      //   console.log('Invalid credentials');
+      //   return null;
+      // },
     }),
   ],
 });
